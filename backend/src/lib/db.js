9d9657db1413884101d000083462a02aa7448b51
uwaje-justice <171641141+uwaje-justice@users.noreply.github.com>
2026-08-17 02:20:04 +0100

    feat: add prisma config, pg adapter client, and prisma scripts

---
+++ backend/src/lib/db.js
@@ -0,0 +1,12 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.js";
+
+const connectionString =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
